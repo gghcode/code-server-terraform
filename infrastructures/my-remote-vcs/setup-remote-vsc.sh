105cb9d105d29884611ae15347af0c45d8671b42
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-06 03:58:41 +0000

    Fix incorrect pemkey name

--- infrastructures/my-remote-vcs/setup-remote-vsc.sh
+++ infrastructures/my-remote-vcs/setup-remote-vsc.sh
@@ -1,9 +1,8 @@
-KEY_NAME=my_workspace_admin22
+KEY_NAME=my_workspace_admin
 
 ssh-keygen -t rsa -b 4096 -C "gyuhwan.a.kim@gmail.com" -f "$HOME/.ssh/$KEY_NAME" -N ""
 
 terraform plan
-
 if [ "$?" -ne "0" ]; then
   echo "terraform plan failed"
   exit 1
