f1a81245af61e192e2ee1d89a8ee6f072bb77dfa
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-06 03:58:08 +0000

    Add process that check installed code-server

--- infrastructures/my-remote-vcs/provision-remote-vsc.sh
+++ infrastructures/my-remote-vcs/provision-remote-vsc.sh
@@ -14,36 +14,35 @@ installed_version=$(code-server --version)
 if [ "$installed_version" = "$CODE_SERVER_VERSION" ];
 then
   echo "Already code-server $CODE_SERVER_VERSION installed..."
-  exit 0
+else
+  wget https://github.com/cdr/code-server/releases/download/$CODE_SERVER_VERSION/$SRC_FILE_NAME
+  if [ "$?" -ne "0" ]; then
+    echo "source download failed..."
+    exit 1
+  fi
+
+  tar -xvzf $SRC_FILE_NAME
+  if [ "$?" -ne "0" ]; then
+    echo "source unzip failed..."
+    exit 2
+  fi 
+
+  cp $SRC_FILE_PATH/code-server $APP_PATH/
+  if [ "$?" -ne "0" ]; then
+    echo "App copy failed..."
+    exit 3
+  fi
+
+  rm -r $SRC_FILE_PATH*
+
+  chmod +x $APP_PATH/code-server
+  if [ "$?" -ne "0" ]; then
+    echo "Can't allow execute permission..."
+    exit 4
+  fi
 fi
 
-wget https://github.com/cdr/code-server/releases/download/$CODE_SERVER_VERSION/$SRC_FILE_NAME
-if [ "$?" -ne "0" ]; then
-  echo "source download failed..."
-  exit 1
-fi
-
-tar -xvzf $SRC_FILE_NAME
-if [ "$?" -ne "0" ]; then
-  echo "source unzip failed..."
-  exit 2
-fi 
-
-cp $SRC_FILE_PATH/code-server $APP_PATH/
-if [ "$?" -ne "0" ]; then
-  echo "App copy failed..."
-  exit 3
-fi
-
-rm -r $SRC_FILE_PATH*
-
-chmod +x $APP_PATH/code-server
-if [ "$?" -ne "0" ]; then
-  echo "Can't allow execute permission..."
-  exit 4
-fi
-
-$APP_PATH/code-server -p $CODE_SERVER_PORT --allow-http
+$APP_PATH/code-server -P $CODE_PWD -p $CODE_SERVER_PORT --allow-http -d ~/.config/Code &
 if [ "$?" -ne "0" ]; then
   echo "code-server start failed..."
   exit 5
