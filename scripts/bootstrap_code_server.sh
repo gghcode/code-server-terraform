ca42938652783ff81de11122eed78a4a7d4a8f08
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-06 14:53:35 +0000

    Implement bootstrap_code_server.sh that run by linux systemctl

--- scripts/bootstrap_code_server.sh
+++ scripts/bootstrap_code_server.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
+
 baseDir=$(dirname "$0")
+userDir=$(dirname "$baseDir")
+password=$1
+port=$2
 
+# Install code-server
 sh $baseDir/get_code_server.sh
-
-code-server -p $CODE_SERVER_PORT -H -d ~/.config/Code
-if [ "$?" -ne "0" ]; then
-  echo "code-server start failed..."
-  exit 5
+if [ "$?" -ne "0" ]; 
+then
+  echo "code-server install failed..."
+  exit 1
 fi
+
+# Setup app conf
+mkdir -p /etc/code-server
+echo "PASSWORD=$password" >> /etc/code-server/code-server.conf
+echo "PORT=$port" >> /etc/code-server/code-server.conf
+
+# Move service file
+userDir=$(dirname "$baseDir")
+
+mv $userDir/code.service /etc/systemd/system/
+
+# Setup systemctl
+systemctl enable code
+systemctl start code
