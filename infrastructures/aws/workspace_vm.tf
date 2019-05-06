ce6fc66e8dbcb07f1ed6dce2a294d7a21d51d126
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-06 13:34:57 +0000

    Update workspace_vm.tf

--- infrastructures/aws/workspace_vm.tf
+++ infrastructures/aws/workspace_vm.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "my_workspace_admin" {
   key_name = "my_workspace_admin"
-  public_key = "${file("${var.ec2_pubkey_path}")}"
+  public_key = "${file("${var.ec2_key_dir}/${var.ec2_key_name}.pub")}"
 }
 
 resource "aws_security_group" "ssh" {
@@ -17,16 +17,31 @@ resource "aws_security_group" "ssh" {
 resource "aws_security_group" "http" {
   name = "allow_http_from_all"
   description = "Allow http port from all"
+  # ingress {
+  #   from_port = 80
+  #   to_port = 80
+  #   protocol = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+  # egress {
+  #   from_port = 80
+  #   to_port = 80
+  #   protocol = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  # Allow all inbound
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -48,19 +63,25 @@ resource "aws_instance" "my_workspace_ec2" {
   connection {
     user        = "${var.ec2_default_user}"
     type        = "ssh"
-    private_key = "${file("${var.ec2_prikey_path}")}"
-    timeout     = "2m"
-  }
-
-  provisioner "file" {
-    source = "${var.src_bootstrap_sh}"
-    destination = "/home/${var.ec2_default_user}/provision-remote-vsc.sh"
+    private_key = "${file("${var.ec2_key_dir}/${var.ec2_key_name}")}"
+    timeout     = "5m"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /home/${var.ec2_default_user}/provision-remote-vsc.sh",
-      "sudo sh /home/${var.ec2_default_user}/provision-remote-vsc.sh",
+      "mkdir -p /home/${var.ec2_default_user}/scripts"
+    ]
+  }
+
+  provisioner "file" {
+    source = "${var.src_scripts_dir}"
+    destination = "/home/${var.ec2_default_user}/scripts/"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/${var.ec2_default_user}/scripts/*sh",
+      "sudo sh /home/${var.ec2_default_user}/scripts/get_code_server.sh"
     ]
   }
 }
