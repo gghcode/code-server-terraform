6b13e9604c835432fb5f92deb04de876b6e54a98
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-06 07:34:22 +0000

    Add shebang line

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -1,17 +1,18 @@
+#!/bin/sh
 dockerVersion=18.06.3~ce~3-0~ubuntu
-  
+
 apt-get purge -y docker lxc-docker docker-engine docker.io
 apt-get install -y curl apt-transport-https ca-certificates \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-apt-get update
-apt-get install docker-ce=$dockerVersion
+apt-get -y update
+apt-get -y install docker-ce=$dockerVersion
 
 # service docker start
 systemctl enable docker
