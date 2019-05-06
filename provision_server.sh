c5a958ac15d474bf2ed12257e1317aeb4deddd49
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-06 14:53:03 +0000

    Add password argument

--- provision_server.sh
+++ provision_server.sh
@@ -1,3 +1,4 @@
+vscPwd=$1
 keyName=my_workspace_admin
 
 scripts/generate_aws_keypem.sh $keyName
@@ -8,4 +9,4 @@ if [ "$?" -ne "0" ]; then
   exit 1
 fi
 
-sh -c "cd infrastructures/aws && terraform apply -auto-approve -var ec2_key_name=$keyName"
+sh -c "cd infrastructures/aws && terraform apply -auto-approve -var ec2_key_name=$keyName -var vsc_password=$vscPwd"
