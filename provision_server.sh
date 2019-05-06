1501182fad0945340739ea0e767b20fd473e6e3a
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-06 05:26:11 +0000

    Move provision_server.sh to root dir

---
+++ provision_server.sh
@@ -0,0 +1,11 @@
+keyName=my_workspace_adm
+
+scripts/generate_aws_keypem.sh $keyName
+
+sh -c "cd infrastructures/aws && terraform plan"
+if [ "$?" -ne "0" ]; then
+  echo "terraform plan failed"
+  exit 1
+fi
+
+sh -c "cd infrastructures/aws && terraform apply -auto-approve -var ec2_key_name=$keyName"
