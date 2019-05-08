e2356a55dc8efcc1ef86ac6976ca859cac45482e
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-08 13:42:49 +0000

    Support tls communication

--- scripts/bootstrap_code_server.sh
+++ scripts/bootstrap_code_server.sh
@@ -13,10 +13,23 @@ then
   exit 1
 fi
 
+# Generate ssl certificate for security
+certPath=~/certs/MyCertificate.crt
+certKeyPath=~/certs/MyKey.key
+
+sh $baseDir/generate_cert.sh
+if [ "$?" -ne "0" ];
+then
+  echo "ssl certificate generate failed..."
+  exit 1
+fi
+
 # Setup app conf
 mkdir -p /etc/code-server
 echo "PASSWORD=$password" >> /etc/code-server/code-server.conf
 echo "PORT=$port" >> /etc/code-server/code-server.conf
+echo "CERT_PATH=$certPath" >> /etc/code-server/code-server.conf
+echo "CERT_KEY_PATH=$certKeyPath" >> /etc/code-server/code-server.conf
 
 # Move service file
 mv $userDir/code.service /etc/systemd/system/
