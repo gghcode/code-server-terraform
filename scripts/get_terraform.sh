214f245b0cfe99ae099325e5fd5867177dcee7a3
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-11 03:58:12 +0000

    Rename variable of terraform version

--- scripts/get_terraform.sh
+++ scripts/get_terraform.sh
@@ -1,25 +1,19 @@
 #!/bin/sh
-
-# Package Install
-# Terraform
-TERRAFORM_VER="0.11.13"
+terraformVersion="0.11.13"
 
 echo "Terraform installing..."
 
-# Cache
-installed_version=`terraform version | sed 's/Terraform v//'`
-if [ "$installed_version" = "$TERRAFORM_VER" ];
+installedVersion=`terraform version | sed 's/Terraform v//'`
+if [ "$installedVersion" = "$terraformVersion" ];
 then 
 	echo "Already terraform installed..."
 else
-	wget -O terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VER}/terraform_${TERRAFORM_VER}_linux_amd64.zip
+	wget -O terraform.zip https://releases.hashicorp.com/terraform/$terraformVersion/terraform_${terraformVersion}_linux_amd64.zip
 
 	unzip terraform.zip
 
 	mv terraform /usr/local/bin/
 	rm terraform.zip
 
-	echo "Terraform installed version ${TERRAFORM_VER}."
+	echo "Terraform installed version ${terraformVersion}."
 fi
-
-# Terraform
