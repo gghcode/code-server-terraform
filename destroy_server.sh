f384a55711b9208f241ed58ef52885b9022f4f8c
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-17 14:27:25 +0000

    Migrate shell interpreter to bash

--- destroy_server.sh
+++ destroy_server.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-baseDir=$(dirname "$0")
-sh -c "cd ${baseDir}/infrastructures/aws && terraform destroy -auto-approve"
+#!/usr/bin/env bash
+BASE_PATH=$(dirname "$0")
+
+bash_c='bash -c'
+
+$bash_c "cd $BASE_PATH/infrastructures/aws && \
+    terraform destroy -auto-approve"
