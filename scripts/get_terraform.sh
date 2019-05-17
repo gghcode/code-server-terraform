98e58c06a12244ca8d8800e5846783419bf324fb
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-17 14:53:19 +0000

    Refactor shell that install terraform

--- scripts/get_terraform.sh
+++ scripts/get_terraform.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
-terraformVersion="0.11.13"
+#!/usr/bin/env bash
+TERRAFORM_VERSION="0.11.13"
 
-echo "Terraform installing..."
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
 
-installedVersion=`terraform version | sed 's/Terraform v//'`
-if [ "$installedVersion" = "$terraformVersion" ];
-then 
+INSTALLED_VERSION=`terraform version 2> /dev/null | sed 's/Terraform v//'`
+if command_exists terraform && [ "$INSTALLED_VERSION" = "$TERRAFORM_VERSION" ]; then 
 	echo "Already terraform installed..."
 else
-	wget -O terraform.zip https://releases.hashicorp.com/terraform/$terraformVersion/terraform_${terraformVersion}_linux_amd64.zip
+	wget -O terraform.zip \
+		https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
 	unzip terraform.zip
 
 	mv terraform /usr/local/bin/
 	rm terraform.zip
 
-	echo "Terraform installed version ${terraformVersion}."
+	echo "Terraform installed version $TERRAFORM_VERSION."
 fi
