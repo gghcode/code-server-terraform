e7d92cc3504ae298317879fb7014b9138d4fa87e
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-17 14:17:46 +0000

    Add flag arguments

--- provision_server.sh
+++ provision_server.sh
@@ -1,32 +1,55 @@
-#!/bin/sh
-baseDir=$(dirname "$0")
-
-vscPwd=$1
-vscPort=$2
-keyName=my_workspace_admin
-
-if [ $# -lt 1 ]; 
-then
-    echo "Require password argument!"
-    exit 1
+#!/usr/bin/env bash
+if [ -z "$VSC_PASSWORD" ]; then
+  echo "Require VSC_PASSWORD env!"
+  exit 1
 fi
 
-if [ -z $2 ];
-then
-  vscPort=80
+DEFAULT_VSC_PORT=80
+if [ -z "$VSC_PORT" ]; then
+    VSC_PORT=$DEFAULT_VSC_PORT
 fi
 
-sh $baseDir/scripts/get_terraform.sh
-sh $baseDir/scripts/generate_aws_keypem.sh $keyName
+DEFAULT_VSC_KEY="my_workspace_admin"
+if [ -z "$VSC_KEY" ]; then
+    VSC_KEY=$DEFAULT_VSC_KEY
+fi
 
-sh -c "cd $baseDir/infrastructures/aws && terraform init && terraform plan"
+POSITIONAL=()
+
+while [ $# -gt 0 ]
+do
+    flag="$1"
+    case $flag in
+        -p|--port)
+        VSC_PORT="$2"
+        shift
+        shift
+        ;;
+        -k|--key)
+        VSC_KEY="$2"
+        shift
+        shift
+        ;;
+    esac
+done
+
+BASE_PATH=$(dirname "$0")
+
+$BASE_PATH/scripts/get_terraform.sh
+$BASE_PATH/scripts/generate_aws_keypem.sh $VSC_KEY
+
+bash_c='bash -c'
+$bash_c "cd $BASE_PATH/infrastructures/aws && \
+    terraform init && \
+    terraform plan"
+
 if [ "$?" -ne "0" ]; then
   echo "terraform plan failed"
   exit 1
 fi
 
-sh -c "cd $baseDir/infrastructures/aws && \
-   terraform apply -auto-approve \
-   -var ec2_key_name=$keyName \
-   -var vsc_password=$vscPwd \
-   -var vsc_port=$vscPort"
+$bash_c "cd $BASE_PATH/infrastructures/aws && \
+    terraform apply -auto-approve \
+    -var ec2_key_name=$VSC_KEY \
+    -var vsc_password=$VSC_PASSWORD \
+    -var vsc_port=$VSC_PORT"
