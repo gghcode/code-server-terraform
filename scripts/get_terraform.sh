194bf8428b877a24c19934a245c2311c56ee3522
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-18 08:35:33 +0000

    Change download command to curl instead of wget for portability

--- scripts/get_terraform.sh
+++ scripts/get_terraform.sh
@@ -9,7 +9,7 @@ INSTALLED_VERSION=`terraform version 2> /dev/null | sed 's/Terraform v//'`
 if command_exists terraform && [ "$INSTALLED_VERSION" = "$TERRAFORM_VERSION" ]; then 
 	echo "Already terraform installed..."
 else
-	wget -O terraform.zip \
+	curl -o terraform.zip \
 		https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
 	unzip terraform.zip
