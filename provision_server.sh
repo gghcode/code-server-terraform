c50fa4a1dacd98bb79bb3a7edb2b763f8cb81289
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-18 14:13:52 +0000

    Extract provisioning function

--- provision_server.sh
+++ provision_server.sh
@@ -37,18 +37,22 @@ BASE_PATH=$(dirname "$0")
 $BASE_PATH/scripts/get_terraform.sh
 $BASE_PATH/scripts/generate_aws_keypem.sh $VSC_KEY
 
-bash_c='bash -c'
-$bash_c "cd $BASE_PATH/infrastructures/aws && \
-  terraform init > /dev/null && \
-  terraform plan"
+do_provisioning() {
+  bash_c='bash -c'
+  $bash_c "cd $BASE_PATH/infrastructures/aws && \
+    terraform init > /dev/null && \
+    terraform plan"
 
-if [ "$?" -ne "0" ]; then
-  echo "terraform plan failed"
-  exit 1
-fi
+  if [ "$?" -ne "0" ]; then
+    echo "terraform plan failed"
+    exit 1
+  fi
 
-$bash_c "cd $BASE_PATH/infrastructures/aws && \
-  terraform apply -auto-approve \
-  -var ec2_key_name=$VSC_KEY \
-  -var vsc_password=$VSC_PASSWORD \
-  -var vsc_port=$VSC_PORT"
+  $bash_c "cd $BASE_PATH/infrastructures/aws && \
+    terraform apply -auto-approve \
+    -var ec2_key_name=$VSC_KEY \
+    -var vsc_password=$VSC_PASSWORD \
+    -var vsc_port=$VSC_PORT"
+}
+
+do_provisioning
