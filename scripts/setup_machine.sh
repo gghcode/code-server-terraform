5ee56e123d5c033c4d1e33b951b9bf3eeb9de092
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-19 10:45:34 +0000

    Add process that install nginx

--- scripts/setup_machine.sh
+++ scripts/setup_machine.sh
@@ -4,11 +4,6 @@
 # ====================
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
 
-DEFAULT_DOCKER_VERSION="18.06.1~ce~3-0~ubuntu"
-if [ -z $DOCKER_VERSION ]; then
-    DOCKER_VERSION=$DEFAULT_DOCKER_VERSION
-fi
-
 get_git() {
     echo "[$DATE] Git installing..."
 
@@ -16,6 +11,11 @@ get_git() {
 
     echo "[$DATE] Git install was successful..."
 }
+
+DEFAULT_DOCKER_VERSION="18.06.1~ce~3-0~ubuntu"
+if [ -z $DOCKER_VERSION ]; then
+    DOCKER_VERSION=$DEFAULT_DOCKER_VERSION
+fi
 
 get_docker() {
     echo "[$DATE] Docker installing..."
@@ -41,9 +41,64 @@ get_docker() {
     echo "[$DATE] Docker install was successful..."
 }
 
+DEFAULT_NGINX_VERSION="1.14.*"
+if [ -z "$NGINX_VERSION" ]; then
+    NGINX_VERSION=$DEFAULT_NGINX_VERSION
+fi
+
+BASE_PATH=$(dirname $0)
+HOME_PATH=$(dirname $BASE_PATH)
+
+get_nginx() {
+    echo "[$DATE] Nginx installing..."
+
+    add-apt-repository -y ppa:nginx/stable &> /dev/null
+
+    apt-get -y update &> /dev/null
+    apt-get install -y nginx=$NGINX_VERSION &> /dev/null
+
+    systemctl enable nginx
+    systemctl start nginx
+
+    echo "[$DATE] Nginx install was successful..."
+}
+
+DEFAULT_CERT_PATH="/etc/ssl/certs"
+if [ -z "VSC_CERT_PATH" ]; then
+    VSC_CERT_PATH=$DEFAULT_CERT_PATH
+fi
+
+CODE_CERT="$VSC_CERT_PATH/code.crt"
+CODE_CERT_KEY="$VSC_CERT_PATH/code.key"
+
+generate_cert() {
+    echo "[$DATE] Certificate generating for SSL..."
+
+    cert_subj="/C=KR/ST=/L=/O=Gyuhwan/OU=/CN=ghcode.dev"
+
+    if [ -f "$CODE_CERT" ]; then
+        if [ -f "$CODE_CERT_KEY" ]; then
+            echo "Skip process because exists certificate..."
+            exit 0
+        fi
+    fi
+
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout $CODE_CERT_KEY -out $CODE_CERT -subj $cert_subj
+    if [ "$?" -ne "0" ]; then
+        echo "certificate generate failed..."
+        exit 1
+    fi
+
+    echo "[$DATE] Certificate generated..."
+}
+
 do_setup() {
+    generate_cert
+
     get_git
     get_docker
+    get_nginx
 }
 
 do_setup
