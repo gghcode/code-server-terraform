6388d43e44db680c3a6d6c275925953c54ecfeab
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-25 17:18:07 +0000

    Add infrastructures of api that access lambda

--- infrastructures/aws/outputs.tf
+++ infrastructures/aws/outputs.tf
@@ -6,6 +6,10 @@ output "dns" {
   value = "${aws_eip.this.public_dns}"
 }
 
+output "api_dns" {
+  value = "${aws_api_gateway_deployment.this.invoke_url}"
+}
+
 # output "instance_id" {
 #   value = "${aws_instance.my_workspace_ec2.*.id}"
 # }
