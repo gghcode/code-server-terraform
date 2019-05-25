5efd9cc67a293a8161f9b75cb7a4a04836c398cc
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-25 02:53:43 +0000

    Disable instance_id output

--- infrastructures/aws/outputs.tf
+++ infrastructures/aws/outputs.tf
@@ -6,6 +6,6 @@ output "dns" {
   value = "${aws_eip.this.public_dns}"
 }
 
-output "instance_id" {
-  value = "${aws_instance.my_workspace_ec2.*.id}"
-}
+# output "instance_id" {
+#   value = "${aws_instance.my_workspace_ec2.*.id}"
+# }
