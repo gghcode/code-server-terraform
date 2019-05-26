671f2e483a08501eac14c28a7aaf9786bf7983f5
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-26 14:49:12 +0000

    Apply terraform fmt

--- infrastructures/aws/api.tf
+++ infrastructures/aws/api.tf
@@ -1,49 +1,44 @@
 resource "aws_api_gateway_rest_api" "this" {
-    name = "CodeScheduleAPI"
+  name = "CodeScheduleAPI"
 }
 
 resource "aws_api_gateway_resource" "this" {
-    rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-    parent_id = "${aws_api_gateway_rest_api.this.root_resource_id}"
-    path_part = "vscode"
+  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
+  parent_id   = "${aws_api_gateway_rest_api.this.root_resource_id}"
+  path_part   = "vscode"
 }
 
 resource "aws_api_gateway_method" "this" {
-    rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-    resource_id = "${aws_api_gateway_resource.this.id}"
-    http_method = "POST"
-    authorization = "NONE"
+  rest_api_id   = "${aws_api_gateway_rest_api.this.id}"
+  resource_id   = "${aws_api_gateway_resource.this.id}"
+  http_method   = "POST"
+  authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "this" {
-    rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-    resource_id = "${aws_api_gateway_method.this.resource_id}"
-    http_method = "${aws_api_gateway_method.this.http_method}"
+  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
+  resource_id = "${aws_api_gateway_method.this.resource_id}"
+  http_method = "${aws_api_gateway_method.this.http_method}"
 
-    integration_http_method = "POST"
-    type = "AWS_PROXY"
-    uri = "${aws_lambda_function.this.invoke_arn}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.this.invoke_arn}"
 }
 
 resource "aws_api_gateway_deployment" "this" {
-    depends_on = [
-        "aws_api_gateway_integration.this"
-    ]
+  depends_on = [
+    "aws_api_gateway_integration.this",
+  ]
 
-    rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-    stage_name = "prod"
+  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
+  stage_name  = "prod"
 }
 
 resource "aws_lambda_permission" "this" {
-    statement_id = "AllowInvokeFromAPIGateway"
-    action = "lambda:InvokeFunction"
-    function_name = "${aws_lambda_function.this.arn}"
-    principal = "apigateway.amazonaws.com"
+  statement_id  = "AllowInvokeFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.this.arn}"
+  principal     = "apigateway.amazonaws.com"
 
-    source_arn = "${aws_api_gateway_rest_api.this.execution_arn}/*/*/*"
+  source_arn = "${aws_api_gateway_rest_api.this.execution_arn}/*/*/*"
 }
-
-
-
-
-
--- infrastructures/aws/outputs.tf
+++ infrastructures/aws/outputs.tf
@@ -13,3 +13,4 @@ output "api_dns" {
 # output "instance_id" {
 #   value = "${aws_instance.my_workspace_ec2.*.id}"
 # }
+
