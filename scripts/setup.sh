997bbb1a297d7fe1e8a35e26e2f7db42f2dc4b05
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-01-07 11:22:15 +0000

    Add process that install ansible on setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env bash
-echo "HI"
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+ensure_installed_ansible() {
+  if ! command_exists 'ansible'; then
+    echo "ansible isn't installed!"
+
+    sudo apt update
+    sudo apt install -y software-properties-common
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt install -y ansible
+  fi
+}
+
+setup() {
+  ensure_installed_ansible
+  echo "HI"
+}
+
+setup
+
